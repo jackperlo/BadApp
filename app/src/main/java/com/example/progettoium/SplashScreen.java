0f5bcea89c5277230f98daaaac917ae3f01e40f6
fabriziosanino <48118048+fabriziosanino@users.noreply.github.com>
2021-10-19 20:32:56 +0200

    Added first version of splash screen

---
+++ app/src/main/java/com/example/progettoium/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.progettoium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Fade;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setExitTransition(new Fade());
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this).toBundle());
+                finishAfterTransition();
+            }
+        }, 500);
+    }
+}
