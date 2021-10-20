83d123fb421c7735caf0ccc2ce0b443bb1a8c411
Giacomo Perlo <giacomo.perlo@edu.unito.it>
2021-10-20 21:08:55 +0200

    Layout Migliorato
    Layout migliorato. Impostazione di tutte le stringhe dal file string.xml, impostati i colori principali dell'app in colors.xml, creato styles.xml in cui sono inseriti i temi custom (vedi quello per personalizzare i button). Aggiunta label di welcome sopra il tabview che viene aggiornata col nome utente quando loggato.

--- app/src/main/java/com/example/progettoium/MainActivity.java
+++ app/src/main/java/com/example/progettoium/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.lifecycle.ViewModelProvider;
@@ -46,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_account, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
