cd477bade167db01ecc58b478e8c200a152ac63a
Giacomo Perlo <giacomo.perlo@edu.unito.it>
2021-11-06 14:06:59 +0100

    Updated Layout for each Course Item
    Aggiunto lo spinner per la scelta dei prof. Per ogni giorno, in ogni ora hai, così, l'elenco dei prof e relativi corsi disponibili. Sarebbe top poi cliccare sul button di Prenotazione e aprire un nuovo fragment che ti dia il dettaglio della prenotazione che stai per effettuare, potrebbe anche fungere da conferma/annullamento della prenotazione stessa.

--- app/src/main/java/com/example/progettoium/ui/home/bookedRepetitions/BookedRepetitionsCustomViewAdapter.java
+++ app/src/main/java/com/example/progettoium/ui/home/bookedRepetitions/BookedRepetitionsCustomViewAdapter.java
@@ -5,13 +5,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.progettoium.R;
@@ -44,7 +48,19 @@ public class BookedRepetitionsCustomViewAdapter extends RecyclerView.Adapter<Boo
         String [] temp = course.getStartTime().split(":");
         int endTime = Integer.parseInt(temp[0]) + 1;
         holder.lbl_CourseEndTime.setText(""+endTime+":00");
-        holder.lbl_CourseDisplayer.setText(""+course.getIDCourse()); //E' UN INT
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Item 1");
+        categories.add("Item 2");
+        categories.add("Item 3");
+        categories.add("Item 4");
+        categories.add("Item 5");
+        categories.add("Item 6");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(mInflater.getContext(), android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        holder.spinner_CoursesDisplayer.setAdapter(dataAdapter);
+        holder.spinner_TeachersDisplayer.setAdapter(dataAdapter);
     }
 
     // total number of rows
@@ -54,17 +70,21 @@ public class BookedRepetitionsCustomViewAdapter extends RecyclerView.Adapter<Boo
     }
 
     // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, AdapterView.OnItemSelectedListener {
         TextView lbl_CourseStartTime;
         TextView lbl_CourseEndTime;
-        TextView lbl_CourseDisplayer;
+        Spinner spinner_CoursesDisplayer;
+        Spinner spinner_TeachersDisplayer;
         Button btn_bookLesson;
 
         ViewHolder(View itemView) {
             super(itemView);
             lbl_CourseStartTime = itemView.findViewById(R.id.lbl_CourseStartTime);
             lbl_CourseEndTime = itemView.findViewById(R.id.lbl_CourseEndTime);
-            lbl_CourseDisplayer = itemView.findViewById(R.id.lbl_CourseDisplayer);
+            spinner_CoursesDisplayer = itemView.findViewById(R.id.spinner_Courses);
+            spinner_CoursesDisplayer.setOnItemSelectedListener(this);
+            spinner_TeachersDisplayer = itemView.findViewById(R.id.spinner_Teachers);
+            spinner_TeachersDisplayer.setOnItemSelectedListener(this);
             btn_bookLesson = itemView.findViewById(R.id.btn_bookLesson);
             btn_bookLesson.setOnClickListener(this);
         }
@@ -72,6 +92,17 @@ public class BookedRepetitionsCustomViewAdapter extends RecyclerView.Adapter<Boo
         @Override
         public void onClick(View view) {
             Toast.makeText(view.getContext(),"CLICK: "+getAdapterPosition(),Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String item = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
+
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
         }
     }
 
